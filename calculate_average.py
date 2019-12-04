7da705a2c00265c1727ed8bcaa4c2fd56dd1fae0
Ilya Bychkov <18483i@gmail.com>
2019-12-04 18:10:39 +0000

    Add logging to the data_handle function

--- calculate_average.py
+++ calculate_average.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import logging
+
+logging.basicConfig(filename = "averages.log", level=logging.DEBUG)
 
 file_name = r"csvFiles\CSVfile_Gen_2019-12-02.csv"
 def data_handle_pd(measure_num = 10, file_name = file_name, min_temp = 20, max_temp=27):
     df = pd.read_csv(file_name)
     calc_df = df.tail(int(measure_num))
-    temp_average = calc_df.loc[:,"Temperature"].mean()
-    print("Setting: {}, {}, {}, {}".format(measure_num,file_name,min_temp,max_temp))
+    temp_average = round(calc_df.loc[:,"Temperature"].mean(), 3)
+    logging.debug("Settings: Measurement number: {}, File name analysed: {}, Low temperature limit: {}C, High temperature limit: {}C".format(measure_num,file_name,min_temp,max_temp))
     if temp_average < min_temp:
         print("It's cold here!")
     elif temp_average > max_temp:
@@ -14,4 +17,4 @@ def data_handle_pd(measure_num = 10, file_name = file_name, min_temp = 20, max_t
         print("The temperature is just right.")
     return temp_average
 
-print("The average temperature is: {}C.".format(data_handle_pd(file_name=file_name, measure_num=200, min_temp=15, max_temp=25)))
+print("The average temperature is: {}C.".format(data_handle_pd(file_name=file_name, measure_num=20, min_temp=15, max_temp=25)))
