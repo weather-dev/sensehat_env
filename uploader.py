4989c22e216f80fb3622610ae4856edd57a291e7
Ilya Bychkov <18483i@gmail.com>
2019-12-06 17:25:15 +0000

    Formatting uploader.py file

--- uploader.py
+++ uploader.py
@@ -1,4 +1,4 @@
-import dropbox 
+import dropbox
 import sys
 import json
 import datetime as dt
@@ -10,24 +10,25 @@ with open("secrets_noupload.json", "r") as secrets_file:
 with open("settings.json", "r") as setting_file:
     setting_data = json.load(setting_file)
 
-logging.basicConfig(filename=setting_data["Uploader"]["log_filename"], level=logging.DEBUG)
+logging.basicConfig(
+    filename=setting_data["Uploader"]["log_filename"], level=logging.DEBUG)
 
-access_token =  secret_data["Secret"]["DBX_tokken"] 
+access_token = secret_data["Secret"]["DBX_tokken"]
 logging.debug("Your access token is: {}".format(access_token))
 dbx = dropbox.Dropbox(access_token)
 
 dbx.users_get_current_account()
 logging.debug("Account data: {}".format(dbx.users_get_current_account()))
 
+
 def uploader():
-    file_date =str( dt.date.today() - dt.timedelta(days = 1) )
+    file_date = str(dt.date.today() - dt.timedelta(days=1))
     filename = "CSVfile_"+file_date+".csv"
     upload_file = 'csvFiles/{}'.format(filename)
     upload_destination = '/csvFiles/Bedroom/'+filename
     with open(upload_file, 'rb') as f:
         dbx.files_upload(f.read(), upload_destination)
-        logging.debug("File: {} uploaded to Dropbox. Location: {}".format(filename, upload_destination))
-
-
+        logging.debug("File: {} uploaded to Dropbox. Location: {}".format(
+            filename, upload_destination))
 
 uploader()
