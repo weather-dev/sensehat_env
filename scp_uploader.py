c21f394d323a2d48402f75667d2707ce6b43a941
Ilya Bychkov <18483i@gmail.com>
2019-12-14 16:11:57 +0000

    Add scp_uploader.py file to upload files to a server

---
+++ scp_uploader.py
@@ -0,0 +1,24 @@
+import os
+import datetime as dt
+import json
+import logging
+
+with open("secret.json", "r") as secret_file:
+    secret_data = json.load(secret_file)
+with open("settings.json", "r") as setting_file:
+    setting_data = json.load(setting_file)
+
+logging.basicConfig(filename=setting_data["scp_send"]["log_filename"], level=logging.DEBUG)
+server_location = secret_data["scp_send"]["server_loc"]
+
+file_date = str( dt.date.today() - dt.timedelta(days = 1) )
+file_name = "CSVfile_{}.csv".format(file_date)
+
+os.chdir(setting_data["envtoCSV"]["output_dir"])
+
+logging.debug(f"Working directory after swap: {os.getcwd()}")
+
+command_run = f"scp {file_name} {server_location}"
+
+os.system(command_run)
+logging.debug(f"Command '{command_run}' run successfully")
